d7b8599aff035561acf8be69cf697b553c5f559b
leedongkyu <leedongkyu0225@gmail.com>
2025-04-22 21:57:13 +0900

    feat: add CmakeList.txt

---
+++ src/client.cpp
@@ -0,0 +1,41 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  struct sockaddr_in serv_addr;
+  int sock;
+  char msg[100];
+
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+
+  if (sock == -1) printf("socket Error\n");
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;  // tcp
+  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_port = htons(7889);
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+    printf("connect Error\n");
+  }
+
+  while (1) {
+    scanf("%s", msg);
+    printf("send: %s\n", msg);
+    write(sock, msg, 100);
+    sleep(1);
+    memset(msg, 0, sizeof(msg));
+  }
+
+  close(sock);
+  return 0;
+}
