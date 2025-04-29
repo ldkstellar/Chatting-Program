1ccdc8381c208a8215bb9fdae6b74684bc635751
leedongkyu <leedongkyu0225@gmail.com>
2025-04-29 03:34:07 +0000

    feat: make chatting program

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,4 @@
 #include <arpa/inet.h>
-#include <iostream>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <string.h>
@@ -7,47 +6,72 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <iostream>
+void *rcv(void *arg) {
+  printf("rcv thread created");
+  int sock = *static_cast<int *>(arg);
+  char buff[500];
+  int len;
+  while (true) {
+    /* code */
+    len = read(sock, buff, sizeof(buff));
+    if (len == -1) {
+      printf("sock close");
+      break;
+    }
+    printf("%s", buff);
+  }
+  pthread_exit(0);
+  return NULL;
+}
 int main(int argc, char const *argv[]) {
-    int sock;
-    struct sockaddr_in serv_addr;
-    pthread_t snd_thread, rcv_thread;
-    void *thread_result;
+  struct sockaddr_in serv_addr;
+  pthread_t snd_thread, rcv_thread;
+  void *thread_result;
 
-    char id[100];
-    strcpy(id, argv[1]);
-    printf("id: %s\n", id);
-
-
-    sock = socket(PF_INET, SOCK_STREAM, 0);
-
-    if (sock == -1)
-        printf("socket Error\n");
-    else {
-        printf("Socket successfully created\n");
-    }
-
-    memset(&serv_addr, 0, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET; // tcp
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(7889);
-
-    if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
-        printf("connect Error\n");
-    } else {
-        printf("Connected\n");
-    }
-    char msg[200];
-    sprintf(msg, "[%s] : hello world\n", id);
-    printf("while before");
-
-    while (true) {
-
-        printf("send: %s\n", msg);
-        write(sock, msg, strlen(msg) + 1);
-        sleep(2);
-    }
-    printf("while after");
-    close(sock);
+  char id[100];
+  if (argc < 2) {
+    printf("you have to enter Id\n");
     return 0;
+  }
+  strcpy(id, argv[1]);
+  printf("id: %s\n", id);
+
+  int sock = socket(PF_INET, SOCK_STREAM, 0);
+
+  if (sock == -1)
+    printf("socket Error\n");
+  else {
+    printf("Socket successfully created\n");
+  }
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;  // tcp
+  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_port = htons(7889);
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+    printf("connect Error\n");
+  } else {
+    printf("Connected\n");
+  }
+  int *sockptr = &sock;
+
+  pthread_create(&rcv_thread, NULL, rcv, static_cast<void *>(sockptr));
+  char chat[100];
+  char msg[200];
+  printf("while before\n");
+
+  while (true) {
+    printf("채팅 입력 : ");
+    fgets(chat, sizeof(chat), stdin);
+    sprintf(msg, "[%s] : %s\n", id, chat);
+    printf("send: %s", msg);
+    write(sock, msg, strlen(msg) + 1);
+    sleep(1);
+  }
+  printf("while after");
+  close(sock);
+  return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -13,57 +13,68 @@
 #define BUFFSIZE 200
 int g_clnt_socks[CLNT_MAX];
 int g_clnt_count = 0;
+void send_all_client(char *msg, int my_sock) {
+  for (size_t i = 0; i < g_clnt_count; i++) {
+    if (g_clnt_socks[i] != my_sock)
+      write(g_clnt_socks[i], msg, strlen(msg) + 1);
+    /* code */
+  }
+}
 void *clnt_connection(void *arg) {
-
-    int clnt_sock = *reinterpret_cast<int *>(arg);
-    char msg[BUFFSIZE];
-    int str_len = 0;
-    int i;
-    while (true) {
-        str_len = read(clnt_sock, msg, sizeof(msg));
-        if (str_len == -1) {
-            printf("clnt[%d] close\n", clnt_sock);
-            break;
-        }
-        printf("%s\n", msg);
+  int clnt_sock = *reinterpret_cast<int *>(arg);
+  char msg[BUFFSIZE];
+  int str_len = 0;
+  int i;
+  while (true) {
+    str_len = read(clnt_sock, msg, sizeof(msg));
+    if (str_len == -1) {
+      printf("clnt[%d] close\n", clnt_sock);
+      break;
     }
-    close(clnt_sock);
-    pthread_exit(0);
-    return NULL;
+    send_all_client(msg, clnt_sock);
+    printf("%s\n", msg);
+  }
+  close(clnt_sock);
+  pthread_exit(0);
+  return NULL;
 }
 int main(int argc, char const *argv[]) {
-    int clnt_sock;
-    int serv_sock;
+  int clnt_sock;
+  int serv_sock;
 
-    pthread_t t_thread;
+  pthread_t t_thread;
 
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
-    int clnt_addr_size;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in clnt_addr;
+  int clnt_addr_size;
 
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    serv_addr.sin_family = AF_INET; // addres
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    // 어떤 주소로 bind 할것인가? ip주소가 바뀔때 마다 자동으로 갱신
-    serv_addr.sin_port = htons(7889); // 포트번호 지정
+  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+  int opt = 1;
+  setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  serv_addr.sin_family = AF_INET;  // addres
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  // 어떤 주소로 bind 할것인가? ip주소가 바뀔때 마다 자동으로 갱신
+  serv_addr.sin_port = htons(7889);  // 포트번호 지정
 
-    if (bind(serv_sock, (const struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
-        printf("bind error \n");
-    }
+  if (bind(serv_sock, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) ==
+      -1) {
+    printf("bind error \n");
+  }
 
-    if (listen(serv_sock, 5) == -1)
-        printf("listen error");
+  if (listen(serv_sock, 5) == -1) printf("listen error");
 
-    char buff[100];
-    int recv_len = 0;
+  char buff[100];
+  int recv_len = 0;
 
-    while (true) {
-        clnt_addr_size = sizeof(clnt_addr);
-        clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_addr, (socklen_t *) &clnt_addr_size);
-        pthread_create(&t_thread, NULL, clnt_connection, (void *)&clnt_sock);
-    }
+  while (true) {
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr,
+                       (socklen_t *)&clnt_addr_size);
+    g_clnt_socks[g_clnt_count++] = clnt_sock;
+    pthread_create(&t_thread, NULL, clnt_connection, (void *)&clnt_sock);
+  }
 
-    // 몇명을 대기 시킬건가
+  // 몇명을 대기 시킬건가
 
-    return 0;
+  return 0;
 }
