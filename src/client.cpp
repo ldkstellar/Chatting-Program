2b489a420179a62330a2846f34fda7f278f54f03
leedongkyu <leedongkyu0225@gmail.com>
2025-04-29 01:43:19 +0000

    feat: using thread for sending other client

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,4 @@
 #include <arpa/inet.h>
-#include <iostream>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <string.h>
@@ -7,47 +6,73 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <iostream>
+
+void *recv(void *arg) {
+  int sock = *reinterpret_cast<int *>(arg);
+  char buff[500];
+  int len;
+  while (true) {
+    len = read(sock, buff, sizeof(buff));
+    if (len == -1) {
+      printf("sock close\n");
+      break;
+    }
+    printf("%s", buff);
+  }
+  pthread_exit(0);
+  return NULL;
+}
+
 int main(int argc, char const *argv[]) {
-    int sock;
-    struct sockaddr_in serv_addr;
-    pthread_t snd_thread, rcv_thread;
-    void *thread_result;
+  int sock;
+  struct sockaddr_in serv_addr;
+  pthread_t rcv_thread;
+  void *thread_result;
 
-    char id[100];
-    strcpy(id, argv[1]);
-    printf("id: %s\n", id);
-
-
-    sock = socket(PF_INET, SOCK_STREAM, 0);
-
-    if (sock == -1)
-        printf("socket Error\n");
-    else {
-        printf("Socket successfully created\n");
-    }
-
-    memset(&serv_addr, 0, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET; // tcp
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(7889);
-
-    if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
-        printf("connect Error\n");
-    } else {
-        printf("Connected\n");
-    }
-    char msg[200];
-    sprintf(msg, "[%s] : hello world\n", id);
-    printf("while before");
-
-    while (true) {
-
-        printf("send: %s\n", msg);
-        write(sock, msg, strlen(msg) + 1);
-        sleep(2);
-    }
-    printf("while after");
-    close(sock);
+  char id[100];
+  if (argc < 2) {
+    printf("you have to enter ID\n");
     return 0;
+  }
+  strcpy(id, argv[1]);
+
+  printf("id: %s\n", id);
+
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+
+  if (sock == -1)
+    printf("socket Error\n");
+  else {
+    printf("Socket successfully created\n");
+  }
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;  // tcp
+  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_port = htons(7889);
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+    printf("connect Error\n");
+  } else {
+    printf("Connected\n");
+  }
+
+  // pthread_create(&rcv_thread, NULL, recv, reinterpret_cast<void *>(sock));
+  char chat[100];
+  char msg[1000];
+
+  printf("while before\n");
+
+  while (true) {
+    std::cin.getline(chat, sizeof(chat));
+    sprintf(msg, "[%s] :%s", id, chat);
+    printf("send: %s\n", msg);
+    write(sock, msg, strlen(msg) + 1);
+    sleep(2);
+  }
+  printf("while after");
+  close(sock);
+  return 0;
 }
--- src/thread.cpp
+++ src/thread.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
 #include <pthread.h>
 #include <unistd.h>
 
+#include <iostream>
 
 pthread_mutex_t mutex;
 
 void *thread1(void *arg) {
-    int a = 0;
+  int a = 0;
 
-    printf("arg: %d\n", reinterpret_cast<int *>(arg));
+  printf("arg: %d\n", reinterpret_cast<int *>(arg));
 
-    while (true) {
-        pthread_mutex_lock(&mutex);
-        printf("thread:%d a[%d]\n", reinterpret_cast<int *>(arg), ++a);
-        pthread_mutex_unlock(&mutex);
-        sleep(2);
-    }
-    return NULL;
+  while (true) {
+    pthread_mutex_lock(&mutex);
+    printf("thread:%d a[%d]\n", reinterpret_cast<int *>(arg), ++a);
+    pthread_mutex_unlock(&mutex);
+    sleep(2);
+  }
+  return NULL;
 }
 // 운영체제마다 스레드 스케줄링 다르다. 염두해 두기
 
 int main(int argc, char *argv[]) {
-    pthread_t s_thread[2];
-    int id = 77;
-    int id2 = 88;
-    pthread_mutex_init(&mutex, NULL);
+  pthread_t s_thread[2];
+  int id = 77;
+  int id2 = 88;
+  pthread_mutex_init(&mutex, NULL);
 
-    pthread_create(&s_thread[0], NULL, thread1, reinterpret_cast<void *>(id));
-    pthread_create(&s_thread[1], NULL, thread1,reinterpret_cast<void *>(id2));
-    while (true) {
-        printf("main loop\n");
-        sleep(1);
-    }
+  pthread_create(&s_thread[0], NULL, thread1, reinterpret_cast<void *>(id));
+  pthread_create(&s_thread[1], NULL, thread1, reinterpret_cast<void *>(id2));
+  while (true) {
+    printf("main loop\n");
+    sleep(1);
+  }
 
-    // pthread_join(s_thread[0], NULL);
-    // pthread_join(s_thread[1], NULL);
-    // 다른스레드가 끝날때까지 기다렸다가 그스레드가 리턴한값을 받아오는 역할
+  // pthread_join(s_thread[0], NULL);
+  // pthread_join(s_thread[1], NULL);
+  // 다른스레드가 끝날때까지 기다렸다가 그스레드가 리턴한값을 받아오는 역할
 }
