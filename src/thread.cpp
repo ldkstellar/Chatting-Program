52d0d7cdecaa6ff83c717da3697b6e2523654055
leedongkyu <leedongkyu0225@gmail.com>
2025-04-29 21:17:01 +0900

    fix:convert casting cppstyle

--- src/thread.cpp
+++ src/thread.cpp
@@ -8,14 +8,15 @@ pthread_mutex_t mutex;
 void *thread1(void *arg) {
   int a = 0;
 
-  printf("arg: %d\n", reinterpret_cast<int *>(arg));
+  printf("arg: %d\n", *static_cast<int *>(arg));
 
   while (true) {
     pthread_mutex_lock(&mutex);
-    printf("thread:%d a[%d]\n", reinterpret_cast<int *>(arg), ++a);
+    printf("thread:%d a[%d]\n", *static_cast<int *>(arg), ++a);
     pthread_mutex_unlock(&mutex);
     sleep(2);
   }
+  pthread_exit(0);
   return NULL;
 }
 // 운영체제마다 스레드 스케줄링 다르다. 염두해 두기
@@ -25,9 +26,11 @@ int main(int argc, char *argv[]) {
   int id = 77;
   int id2 = 88;
   pthread_mutex_init(&mutex, NULL);
+  int *id_ptr = &id;
+  int *id2_ptr = &id2;
 
-  pthread_create(&s_thread[0], NULL, thread1, &id);
-  pthread_create(&s_thread[1], NULL, thread1, &id2);
+  pthread_create(&s_thread[0], NULL, thread1, static_cast<void *>(id_ptr));
+  pthread_create(&s_thread[1], NULL, thread1, static_cast<void *>(id2_ptr));
   while (true) {
     printf("main loop\n");
     sleep(1);
